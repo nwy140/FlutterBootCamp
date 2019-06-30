fd5c896647e673d8ab4400ea7e6e5fdffec5606f
Nwy <nwy140@gmail.com>
2019-06-30 15:35:06 -0600

    V0.0.2b I_AM_RICH Flutter Project ChangeLog - Added AppBar - Learned that AppBar must be hot restarted instead of hotreloaded on flutter

--- i_am_rich/lib/main.dart
+++ i_am_rich/lib/main.dart
@@ -1,18 +1,13 @@
-// Flutter code sample for material.Scaffold.1
-
-// This example shows a [Scaffold] with an [AppBar], a [BottomAppBar] and a
-// [FloatingActionButton]. The [body] is a [Text] placed in a [Center] in order
-// to center the text within the [Scaffold] and the [FloatingActionButton] is
-// centered and docked within the [BottomAppBar] using
-// [FloatingActionButtonLocation.centerDocked]. The [FloatingActionButton] is
-// connected to a callback that increments a counter.
-
 import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: Center(
-          child: Text("hello world"),
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text(
+                "I AM RICH, please use Hot Restart instead of Hot Reload for appBar"), // Flutter doesn't allow you to hot reload app titles, use hot restart
+            backgroundColor: Colors.blueGrey,
+          ), // Scaffold widget
         ),
       ),
     );
