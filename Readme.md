0ac37db1e0e57ba00a4f357b052dbfbac872ea17
Nwy <nwy140@gmail.com>
2019-06-30 17:50:57 -0600

    V0.0.

--- Readme.md
+++ Readme.md
@@ -1,3 +1,10 @@
 # Flutter Documentation Repo
 
-https://github.com/londonappbrewery/Flutter-Course-Resources
+https://github.com/londonappbrewery/Flutter-Course-Resources	
+
+https://github.com/Solido/awesome-flutter
+
+## Flutter Material Library Documentation
+
+https://api.flutter.dev/flutter/material/material-library.html
+
--- i_am_rich/lib/main.dart
+++ i_am_rich/lib/main.dart
@@ -6,17 +6,16 @@ void main() => runApp(
           backgroundColor: Colors.black45,
           appBar: AppBar(
             title: Text(
-                "I AM RICH, V2 please use Hot Restart instead of Hot Reload for appBar"), // Flutter doesn't allow you to hot reload app titles, use hot restart
+                "I AM RICH"), // Flutter doesn't allow you to hot reload app titles, use hot restart
             backgroundColor: Colors.blueGrey,
           ), // Scaffold widget
           body: Center(
             child: Image(
-              image: NetworkImage(
-                'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_92x30dp.png',
+              image: AssetImage(
+                'images/diamond.png',
               ),
             ),
           ),
         ),
       ),
     );
-//
