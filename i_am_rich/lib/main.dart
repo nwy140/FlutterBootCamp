24645b0e1a341f82978f31cc215d74260a68fbb9
Nwy <nwy140@gmail.com>
2019-06-30 17:19:17 -0600

    q

--- i_am_rich/lib/main.dart
+++ i_am_rich/lib/main.dart
@@ -3,11 +3,20 @@ import 'package:flutter/material.dart';
 void main() => runApp(
       MaterialApp(
         home: Scaffold(
+          backgroundColor: Colors.black45,
           appBar: AppBar(
             title: Text(
-                "I AM RICH, please use Hot Restart instead of Hot Reload for appBar"), // Flutter doesn't allow you to hot reload app titles, use hot restart
+                "I AM RICH, V2 please use Hot Restart instead of Hot Reload for appBar"), // Flutter doesn't allow you to hot reload app titles, use hot restart
             backgroundColor: Colors.blueGrey,
           ), // Scaffold widget
+          body: Center(
+            child: Image(
+              image: NetworkImage(
+                'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_92x30dp.png',
+              ),
+            ),
+          ),
         ),
       ),
     );
+//
